6348d83b878be1605b5b7a19bebf9e2b1aec236a
Obinna Nwabia <valinevite@gmail.com>
2020-11-20 22:11:36 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+def reverse_each_word(string)
+    string_1 = string.split(" ")
+    word = []
+    string_1.each do |word_2|
+        word << word_2.reverse
+    end
+    p word.join(" ")
+end
+
+def reverse_each_word(string)
+    string_1 = string.split(" ")
+    word = []
+    string_1.collect do |word_2|
+        word << word_2.reverse
+    end
+    p word.join(" ")
+end
